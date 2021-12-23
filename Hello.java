4a1ea41a691175571f678aed02ce621f02051466
Pooja99Kulkarni <94114652+Pooja99Kulkarni@users.noreply.github.com>
2021-12-23 17:22:09 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public static void main(String [] args){
    int a =0;
 System.out.println("Hello World");
   System.out.println("Hi!! I am Pooja");
+ System.out.println("demo");
   if(a==0){
     System.out.println("OK");
    System.out.println("try");
